58c600dd4f9c21a5a0a83562e1398bd4f6a3d113
Narador <35881757+Narador@users.noreply.github.com>
2018-09-10 21:57:09 -0500

    Created server models - added db to gitignore for pulls/forks

--- artecosab/calculator/models.py
+++ artecosab/calculator/models.py
@@ -1,3 +1,26 @@
+'''
+Models for the calculator app
+These models determine what fields are
+used in showing server and camera fields
+'''
+
 from django.db import models
 
-# Create your models here.
+class Server(models.Model):
+    ''' Server class to show server info on homepage and 
+    the ability to add cameras to an instance of a Server
+    '''
+
+    server_name = models.CharField(max_length=50)
+
+    total_space = models.PositiveIntegerField()
+
+    ''' TODO
+
+    # of cameras = #
+
+    min # of days -- this could be the minimum # of days any camera has
+    '''
+
+    def __str__(self):
+        return self.server_name
